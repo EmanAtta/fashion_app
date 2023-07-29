883626e0b363992bdbbba9fccc894ff85dcdd06b
norwagdy16 <139642418+norwagdy16@users.noreply.github.com>
2023-07-29 19:56:26 +0300

    nor2

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:homelayout(email: 'email', email1: 'email1', fullname: 'fullname', username: 'username'),
+      home:first_page(),
     );
   }
 }
